aaac0017b51e7dec4917dfd282dbb1b62c2629fb
Zhenggang Su <zgsu@alauda.io>
2021-07-02 15:12:28 +0800

    chore: add app/config

--- src/demo.ts
+++ src/demo.ts
@@ -1,27 +1,33 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
-const URL = 'https://www.iconfont.cn/';
+const URL = "https://www.iconfont.cn/";
 const VIEWPORT = { height: 1920, width: 1280 };
 
-(async() => {
-  const browser = await puppeteer.launch({headless: false});
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto(URL, { waitUntil: 'networkidle0' });
+  await page.goto(URL, { waitUntil: "networkidle0" });
 
   // Resize the viewport to screenshot elements outside of the viewport
-  const bodyHandle = await page.$('body');
+  const bodyHandle = await page.$("body");
   const boundingBox = await bodyHandle?.boundingBox();
   const newViewport = {
-    width: Math.max(VIEWPORT.width, Math.ceil((boundingBox as puppeteer.BoundingBox).width)),
-    height: Math.max(VIEWPORT.height, Math.ceil((boundingBox as puppeteer.BoundingBox).height)),
+    width: Math.max(
+      VIEWPORT.width,
+      Math.ceil((boundingBox as puppeteer.BoundingBox).width)
+    ),
+    height: Math.max(
+      VIEWPORT.height,
+      Math.ceil((boundingBox as puppeteer.BoundingBox).height)
+    ),
   };
-  await page.setViewport(Object.assign({}, VIEWPORT, newViewport));
+  await page.setViewport({ ...VIEWPORT, ...newViewport });
 
   // find signin
-  const signInElmDom = await page.waitForSelector('header .signin');
+  const signInElmDom = await page.waitForSelector("header .signin");
   await signInElmDom?.click();
-  await page.waitForNavigation({waitUntil: 'load'});
+  await page.waitForNavigation({ waitUntil: "load" });
 
   // await page.screenshot({path: 'screenshot/example.png', fullPage: true});
   await browser.close();
-})()
+})();
