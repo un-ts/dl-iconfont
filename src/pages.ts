2563037bc73724de7fd377fa65081531219dddf3
Zhenggang Su <zgsu@alauda.io>
2021-07-02 17:37:23 +0800

    chore: find dialog

--- src/pages.ts
+++ src/pages.ts
@@ -11,23 +11,37 @@ const VIEWPORT = { height: 1920, width: 1280 };
   // Resize the viewport to screenshot elements outside of the viewport
   const bodyHandle = await page.$("body");
   const boundingBox = await bodyHandle?.boundingBox();
+  const currentWidth = Math.max(
+    VIEWPORT.width,
+    Math.ceil((boundingBox as puppeteer.BoundingBox).width)
+  );
+  const currentHeight = Math.max(
+    VIEWPORT.height,
+    Math.ceil((boundingBox as puppeteer.BoundingBox).height)
+  );
   const newViewport = {
-    width: Math.max(
-      VIEWPORT.width,
-      Math.ceil((boundingBox as puppeteer.BoundingBox).width)
-    ),
-    height: Math.max(
-      VIEWPORT.height,
-      Math.ceil((boundingBox as puppeteer.BoundingBox).height)
-    ),
+    width: currentWidth,
+    height: currentHeight,
   };
   await page.setViewport({ ...VIEWPORT, ...newViewport });
 
   // find signin
-  const signInElmDom = await page.waitForSelector("header .signin");
-  await signInElmDom?.click();
-  await page.waitForNavigation({ waitUntil: "load" });
+  const signInEleDom = await page.waitForSelector("header .signin");
+  await signInEleDom?.click();
 
-  await page.screenshot({ path: "screenshot/example.png", fullPage: true });
+  // find github login
+  await page.waitForSelector(".show-dialog", { visible: true });
+  const githubLoginEleDom = await page.waitForSelector(
+    ".mp-e2e-content .github"
+  );
+  await githubLoginEleDom?.click();
+
+  await page.waitForNavigation({ waitUntil: "networkidle0" });
+
+  // screenshot
+  await page.screenshot({
+    path: "screenshot/example.png",
+    fullPage: true,
+  });
   await browser.close();
 })();
