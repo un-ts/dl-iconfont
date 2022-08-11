e1fad78d77b2f3194fba693f72dfffed32d5bea2
JounQin <admin@1stg.me>
2022-08-11 05:55:12 +0000

    feat: use top level await

--- src/cli.ts
+++ src/cli.ts
@@ -12,27 +12,20 @@ const {
 
 const DOWNLOAD_FILE = process.argv[2] || ICONFONT_DOWNLOAD_FILE
 
-const main = async () => {
-  const jsUrl = await fetchJsUrl({
-    projectId: ICONFONT_PROJECT_ID!,
-    login: ICONFONT_LOGIN!,
-    password: ICONFONT_PASSWORD!,
-    headless: !['0', 'false'].includes(PUPPETEER_HEADLESS!),
-  })
-  if (DOWNLOAD_FILE) {
-    console.log(
-      `[dl-iconfont]: downloading \`${jsUrl!}\` into \`${DOWNLOAD_FILE}\``,
-    )
-    await download(jsUrl!, DOWNLOAD_FILE)
-  } else {
-    console.log(
-      '[dl-iconfont] no `ICONFONT_DOWNLOAD_FILE` provided, you can use the environment variable or `dli ICONFONT_DOWNLOAD_FILE` to download the js file automatically',
-    )
-    console.log(`[dl-iconfont]: ${jsUrl!}`)
-  }
-}
-
-main().catch((err: Error) => {
-  console.error(err)
-  process.exitCode = 1
+const jsUrl = await fetchJsUrl({
+  projectId: ICONFONT_PROJECT_ID!,
+  login: ICONFONT_LOGIN!,
+  password: ICONFONT_PASSWORD!,
+  headless: !['0', 'false'].includes(PUPPETEER_HEADLESS!),
 })
+if (DOWNLOAD_FILE) {
+  console.log(
+    `[dl-iconfont]: downloading \`${jsUrl!}\` into \`${DOWNLOAD_FILE}\``,
+  )
+  await download(jsUrl!, DOWNLOAD_FILE)
+} else {
+  console.log(
+    '[dl-iconfont] no `ICONFONT_DOWNLOAD_FILE` provided, you can use the environment variable or `dli ICONFONT_DOWNLOAD_FILE` to download the js file automatically',
+  )
+  console.log(`[dl-iconfont]: ${jsUrl!}`)
+}
